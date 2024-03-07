72aaf412940b6739cf61dd1dfb8e9d5cf20a857d
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-07 13:00:33 +0200

    Add new field tag to Task model

--- todo_list/models.py
+++ todo_list/models.py
@@ -5,6 +5,11 @@ class Task(models.Model):
     content = models.TextField()
     datetime = models.DateTimeField(auto_now_add=True)
     is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(
+        to="todo_list.Tag",
+        related_name="tags",
+        blank=True
+    )
 
 
 class Tag(models.Model):
