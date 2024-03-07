e810486226521817d7c84deb2ec7c504e655d6e1
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-07 12:53:46 +0200

    Add new model Task

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
