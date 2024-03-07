351bf6d937137de31cc08af2482719b12d303680
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-03-07 12:55:12 +0200

    Add mew model Tag

--- todo_list/models.py
+++ todo_list/models.py
@@ -5,3 +5,7 @@ class Task(models.Model):
     content = models.TextField()
     datetime = models.DateTimeField(auto_now_add=True)
     is_completed = models.BooleanField(default=False)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=65, unique=True)
